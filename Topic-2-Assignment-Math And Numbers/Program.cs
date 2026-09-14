0c39ffd3abe01caef7aab839a3bae1305906a43e
Easton <362585226@2260I-L29-032>
2026-09-14 13:08:37 -0400

    Started the assignment

--- Topic-2-Assignment-Math And Numbers/Program.cs
+++ Topic-2-Assignment-Math And Numbers/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Easton Powers");
+            Console.WriteLine(" My Year Of Birth Is " + (2026 - 17));
+            Console.WriteLine("Average of 14, 15, 16 is " + ((14 + 15 + 16) / 3));
+            Console.WriteLine("Average of 14, 15 ,16 ,17 is " + ((14 + 15 + 16 + 17) / 4));
         }
     }
 }
