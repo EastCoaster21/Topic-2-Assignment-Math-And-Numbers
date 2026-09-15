ade1f11afb2af54bede3fdbcae833d3ad902876f
Easton <362585226@2260I-L29-032>
2026-09-15 12:34:05 -0400

    90% finished the assignment

--- Topic-2-Assignment-Math And Numbers/Program.cs
+++ Topic-2-Assignment-Math And Numbers/Program.cs
@@ -5,9 +5,20 @@
         static void Main(string[] args)
         {
             //Console.WriteLine("Easton Powers");
-            Console.WriteLine(" My Year Of Birth Is " + (2026 - 17));
+            Console.WriteLine("Math and Numbers");
+            Console.WriteLine();
+            Console.WriteLine("My Year Of Birth Is " + (2026 - 17));
+            Console.WriteLine();
             Console.WriteLine("Average of 14, 15, 16 is " + ((14 + 15 + 16) / 3));
-            Console.WriteLine("Average of 14, 15 ,16 ,17 is " + ((14 + 15 + 16 + 17) / 4));
+            Console.WriteLine();
+            Console.WriteLine("Average of 14, 15 ,16 ,17 is " + ((14.0 + 15.0 + 16.0 + 17.0) / 4));
+            Console.WriteLine();
+            Console.WriteLine("The area of a circle where r=3cm is " + Math.Round((Math.PI) * Math.Pow(3, 2), 1));
+            Console.WriteLine();
+            Console.WriteLine("Press ENTER to continue");
+            Console.WriteLine();
+            //Console.Clear();
+            Console.WriteLine("Why does ");
         }
     }
 }
